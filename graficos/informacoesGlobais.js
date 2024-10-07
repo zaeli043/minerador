ce052e5e3b7db33e2caf1749be58e391dbf07a07
zaeli043 <144798250+zaeli043@users.noreply.github.com>
2024-10-07 16:28:02 -0300

    Add files via upload

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -5,13 +5,19 @@ async function vizulizarInformacaoGlobal(){
     const res = await fetch(url)
 const dados = await res.json()
 console.log(dados)
+const pessoasNomundo = (dados.total_pessoas_mundo / 1e9)
+const horas = parseInt(dados.tempo_medio)
+const minutos = Math.round((dados.tempo_medio - horas)*100)
 
 const paragrafo = document.createElement('p')
 
 paragrafo.classList.add('graficos-container__texto')
 
 paragrafo.innerHTML = `Neymar
-${dados.total_pessoas_conectadas}`
+<span> ${pessoasNomundo} </span> de pessoas e aproximadamente
+<span> ${dados.total_pessoas_conectadas} </span> estão conectadas em alguma rede social e passam em média
+<span> ${horas} </span> : <span>${minutos} </span> horas conectadas`
+
 
 const container = document.getElementById("grafico-container")
 container.appendChild(paragrafo)
